03bf812ee9feb3193050ea44b09c45b7ad88d975
cometbroom <alawi.wow@gmail.com>
2022-02-06 15:19:47 +0100

    Create board function

---
+++ src/app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const gBoard = document.querySelector(".game");
+    const topMsg = document.querySelector(".top-msg h2");
+    let blocks = [];
+
+    topMsg.innerHTML = "Welcome";
+    (function createBoard() {
+        for (let i = 0; i < 15; ++i) {
+            let block = createBlock("block", i + 1);
+            blocks.push(block);
+            gBoard.appendChild(block);
+        }
+        let finalBlock = createBlock("block", "");
+        finalBlock.classList.add("empty");
+        blocks.push(finalBlock);
+        gBoard.appendChild(finalBlock);
+    })();
+});
+
+function createBlock(cls, inner) {
+    let block = document.createElement("div");
+
+    block.classList.add(cls);
+    block.innerHTML = inner;
+
+    return block;
+}
