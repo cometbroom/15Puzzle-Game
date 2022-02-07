cb7bbf8d8e9a298cf6d53c49283ba8a59351af9f
cometbroom <alawi.wow@gmail.com>
2022-02-07 17:13:24 +0100

    Implemented shuffle function

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const gBoard = document.querySelector(".p15");
-    const topMsg = document.querySelector(".top-msg h2");
-    let blocks = [];
+let blocks = [];
+let gBoard;
+let topMsg;
 
-    topMsg.innerHTML = "Welcome";
-    (function createBoard() {
-        for (let i = 0; i < 15; ++i) {
-            let block = createBlock("block", i + 1);
-            blocks.push(block);
-            gBoard.appendChild(block);
-        }
-        let finalBlock = createBlock("block", "");
-        finalBlock.classList.add("empty");
-        blocks.push(finalBlock);
-        gBoard.appendChild(finalBlock);
-    })();
-});
+let pos = { x: 3, y: 3 };
+function createBoard() {
+    for (let i = 0; i < 15; ++i) {
+        let block = createBlock("block", i + 1);
+        blocks.push(block);
+        gBoard.appendChild(block);
+    }
+    let finalBlock = createBlock("block", 0);
+    finalBlock.classList.add("empty");
+    blocks.push(finalBlock);
+    gBoard.appendChild(finalBlock);
+}
 
 function createBlock(cls, inner) {
     let block = document.createElement("div");
@@ -25,3 +23,56 @@ function createBlock(cls, inner) {
 
     return block;
 }
+
+function shuffleBoard() {
+    let moveArr = [];
+    for (let i = 0; i < 1000; ++i) {
+        possibleMove(moveArr);
+        let randIndex = Math.floor(Math.random() * moveArr.length);
+        move(moveArr[randIndex]);
+        moveArr = [];
+    }
+}
+
+function move(dir) {
+    let p = pos.x + pos.y * 4;
+    switch (dir) {
+        case 1:
+            pos.y--;
+            break;
+        case 2:
+            pos.x++;
+            break;
+        case 4:
+            pos.y++;
+            break;
+        case 8:
+            pos.x--;
+            break;
+    }
+    blocks[p].innerHTML = blocks[pos.x + pos.y * 4].innerHTML;
+    blocks[pos.x + pos.y * 4].innerHTML = 0;
+}
+
+function possibleMove(moveArr) {
+    if (pos.x < 3) moveArr.push(2);
+    if (pos.x > 0) moveArr.push(8);
+    if (pos.y < 3) moveArr.push(4);
+    if (pos.y > 0) moveArr.push(1);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    gBoard = document.querySelector(".p15");
+    topMsg = document.querySelector(".top-msg h2");
+    topMsg.innerHTML = "Welcome";
+    createBoard();
+    shuffleBoard();
+    /*
+	- When clicked on a block, see which side has empty block and move it there
+	- Randomize numbers with unique random function
+	- 
+
+
+
+	*/
+});
